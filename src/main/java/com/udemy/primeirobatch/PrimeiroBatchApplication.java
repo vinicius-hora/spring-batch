4a8870b06ed4f3e218378d35a9dfeb71d48183a3
vinicius-hora <vinicius_hora@live.com>
2023-09-26 16:22:38 -0300

    teste

---
+++ src/main/java/com/udemy/primeirobatch/PrimeiroBatchApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.primeirobatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiroBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiroBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udemy/primeirobatch/PrimeiroBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.primeirobatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
